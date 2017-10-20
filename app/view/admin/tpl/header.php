63d3f9a18119398622d253a2b61199bc8fef7e11
carloboy16 <carloboyjcs16@gmail.com>
2017-10-20 17:02:42 +0800

    update

--- app/view/admin/tpl/header.php
+++ app/view/admin/tpl/header.php
@@ -8,5 +8,7 @@
 	<link rel="stylesheet" href="/assets/css/bootstrap.css" >
 		<link rel="stylesheet" href="/assets/css/admin.css" >
 	<link rel="stylesheet" href="/assets/css/bootstrap-theme.min.css" >
+	
+	<script src="/assets/js/jquery-3.2.1.min.js" ></script>
 	<script src="/assets/js/bootstrap.min.js" ></script>
 </head>
